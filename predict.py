5c4d78bc5c1fbb0cc86928a596e2f5b74a245457
Hoel Bagard <hoel.bagard@gmail.com>
2021-05-28 18:31:44 +0900

    Tried to integrate the frame fusing into the inference, untested.

--- predict.py
+++ predict.py
@@ -83,115 +83,162 @@ class YolactK:
         if imgs.ndim == 3:
             imgs = np.expand_dims(imgs, axis=0)
             img_paths = np.expand_dims(img_paths, axis=0) if img_paths else None
+        if self.output_dir_path:
+            assert img_paths, "If saving results as images, the image paths must be provided"
         if img_paths is None:
             img_paths = list(np.arange(len(imgs)))
 
-        list_of_centers = []
-
+        imgs_masks, imgs_bboxes = [], []
         with torch.no_grad():
             for img, img_path in zip(imgs, img_paths):
-                if self.output_dir_path:
-                    assert img_path, "If saving results as images, the image path must be provided"
-                    output_path = (self.output_dir_path / img_path.name).with_suffix(".png")
+                masks, bboxes = self.run_network(img)
+                imgs_masks.append(masks)
+                imgs_bboxes.append(bboxes)
 
-                # List that will store all the hair center for this image
-                img_centers = []
+        if fuse_results:
+            imgs_masks, imgs_bboxes = fuse_outputs(imgs_masks)
 
-                # Run image through the network
-                img_gpu = torch.from_numpy(img).cuda().float()
-                batch = FastBaseTransform()(img_gpu.unsqueeze(0))
-                preds = self.net(batch)
-                h, w, _ = img.shape
+        result: list[list[tuple[int, int]]] = []   # List with all the detected centers for each image.
+        for i in range(len(imgs)):
+            bboxes = imgs_bboxes if fuse_results else imgs_bboxes[i]
+            masks = imgs_masks if fuse_results else imgs_masks[i]
 
-                # Post process
-                t = postprocess(preds, w, h, visualize_lincomb=True, crop_masks=True, score_threshold=0.15)
-                top_k = 15  # Further restrict the number of predictions to parse
-                idx = t[1].argsort(0, descending=True)[:top_k]
-                masks = t[3][idx].cpu().numpy()
-                classes, scores, boxes = [x[idx].cpu().numpy() for x in t[:3]]
+            closest_points, end_points, center_points, bb_centers = [], [], [], []
+            for detection_index in range(len(bboxes)):
+                top_x, top_y, width, height = bboxes[detection_index]
+                # Make sure that the bounding boxes are not going outside the image
+                width = min(width, img.shape[0] - top_x)
+                height = min(height, img.shape[1] - top_y)
+                bb_center = (top_x + width//2, top_y + height//2)
+                if self.verbose:
+                    print(f"bounding box center: {bb_center}")
 
-                # Visualization
-                for detection_index in range(len(boxes)):
-                    # if self.verbose:
-                    #     print(f"Number of non zero pixels in mask: {np.sum(masks[detection_index])}")
+                closest_point, end_point, center_point = self.get_points(masks[detection_index], bb_center)
 
-                    # Draw bounding box on the image
-                    top_x, top_y, width, height = boxes[detection_index]
-                    # Make sure that the bounding boxes are not going outside the image
-                    width = min(width, img.shape[0] - top_x)
-                    height = min(height, img.shape[1] - top_y)
-                    center_x, center_y = top_x + width//2, top_y + height//2
-                    if self.verbose:
-                        print(f"bounging box center: {(center_x, center_y)}")
-                    if self.output_dir_path:
-                        img = cv2.rectangle(img, (top_x, top_y), (top_x+width, top_y+height), (0, 0, 255), 5)
+                closest_points.append(center_point)
+                end_points.append(center_point)
+                center_points.append(center_point)
+                bb_centers.append(bb_center)
 
-                    if self.output_dir_path:
-                        # Add the mask to the image
-                        color = np.random.random(3)*255  # (0, 0, 255)
-                        img[masks[detection_index].astype(np.bool8)] = color
+            result.append(center_points)
 
-                    # Get the point on the mask that is the closest to the bounding box's center
-                    # Super inefficient but should work so....
-                    dists = np.empty_like(masks[detection_index])
-                    for i in range(dists.shape[0]):
-                        for j in range(dists.shape[1]):
-                            dists[i][j] = (center_y - i)**2 + (center_x - j)**2
-                    dists = np.where(masks[detection_index], dists, np.inf)
-                    closest_point = np.unravel_index(dists.argmin(), dists.shape)
-                    if self.verbose:
-                        print(f"{closest_point=}")
+            if self.output_dir_path:
+                self.draw_on_image(img, img_path, bboxes, masks, closest_points, end_points, center_points)
 
-                    # If the bounding box's center is already on the mask, then just return that
-                    if closest_point == (center_y, center_x):
-                        if self.verbose:
-                            print("Bounding box's center was already on the mask")
-                        center = end_point = closest_point
-                    # Otherwise, "trace" a line going through the bounding box's center and the closest point on the
-                    # mask. We start inspecting that line from the closest point and in the direction going away from
-                    # the bounding box's center. Stops when the line goes out of the mask.
-                    else:
-                        # Get unit vector
-                        line_dir = np.asarray([closest_point[0]-center_y, closest_point[1]-center_x])
-                        if self.verbose:
-                            print(f"line_dir before normalization: {line_dir}")
-                        line_dir = line_dir / np.amax(np.abs(line_dir))
-                        if self.verbose:
-                            print(f"Normalized line_dir: {line_dir}")
-                        mult_factor = 1
-                        current_x = closest_point[0] + int(line_dir[0] * mult_factor)
-                        current_y = closest_point[1] + int(line_dir[1] * mult_factor)
-                        while (0 <= current_x < img.shape[0]
-                                and 0 <= current_y < img.shape[1]
-                                and masks[detection_index, current_x, current_y]):
-                            current_x = closest_point[0] + int(line_dir[0] * mult_factor)
-                            current_y = closest_point[1] + int(line_dir[1] * mult_factor)
-                            mult_factor += 1
+        return result
 
-                        center = closest_point + (line_dir * mult_factor/2).astype(int)
-                        end_point = closest_point + (line_dir * mult_factor).astype(int)
+    def draw_on_image(self, img: np.ndarray, img_path: Path, bboxes: np.ndarray, masks: np.ndarray,
+                      closest_points: list[tuple[int, int]], end_points: list[tuple[int, int]],
+                      center_points: list[tuple[int, int]], bb_centers: list[tuple[int, int]]) -> None:
+        """ Draws the bboxes, masks and points of interest on the image and then saves it. """
+        point_size = 3
+        output_path = (self.output_dir_path / img_path.name).with_suffix(".png")
 
-                    if self.verbose:
-                        print(f"{center=}")
+        for_loop_args = zip(bboxes, masks, closest_points, end_points, center_points, bb_centers)
+        for bb_box, mask, closest_point, end_point, center_point, bb_center in for_loop_args:
+            # Add the bounging box to the image
+            top_x, top_y, width, height = bb_box
+            img = cv2.rectangle(img, (top_x, top_y), (top_x+width, top_y+height), (0, 0, 255), 5)
 
-                    if self.output_dir_path:
-                        point_size = 3
-                        # Add closest point on image
-                        img = cv2.circle(img, (closest_point[1], closest_point[0]), point_size, (0, 255, 0), point_size)
-                        # Add line end point
-                        img = cv2.circle(img, (end_point[1], end_point[0]), point_size, (255, 255, 255), point_size)
-                        # Add found center
-                        img = cv2.circle(img, (center[1], center[0]), point_size, (255, 0, 0), point_size)
-                        # Add bounding box center
-                        img = cv2.circle(img, (center_x, center_y), point_size, (0, 0, 0), point_size)
+            # Add the mask to the image
+            color = np.random.random(3)*255  # (0, 0, 255)
+            img[mask.astype(np.bool8)] = color
 
-                    img_centers.append(center)
+            # Add closest point on image
+            img = cv2.circle(img, (closest_point[1], closest_point[0]), point_size, (0, 255, 0), point_size)
+            # Add line end point
+            img = cv2.circle(img, (end_point[1], end_point[0]), point_size, (255, 255, 255), point_size)
+            # Add found center
+            img = cv2.circle(img, (center_point[1], center_point[0]), point_size, (255, 0, 0), point_size)
+            # Add bounding box center
+            img = cv2.circle(img, (bb_center, bb_center), point_size, (0, 0, 0), point_size)
+        cv2.imwrite(str(output_path), img)
 
-                if self.output_dir_path:
-                    cv2.imwrite(str(output_path), img)
-                list_of_centers.append(img_centers)
+    def run_network(self, img: np.ndarray):
+        """ Runs an image through the network + postprocessing and returns the masks and bboxes
 
-        return list_of_centers
+        Args:
+            img (np.ndarray): The image to process.
+
+        Returns:
+            (tuple): the masks and bboxes
+        """
+        # Run image through the network
+        img_gpu = torch.from_numpy(img).cuda().float()
+        batch = FastBaseTransform()(img_gpu.unsqueeze(0))
+        preds = self.net(batch)
+        h, w, _ = img.shape
+
+        # Post process
+        t = postprocess(preds, w, h, visualize_lincomb=True, crop_masks=True, score_threshold=0.15)
+        top_k = 15  # Further restrict the number of predictions to parse
+        idx = t[1].argsort(0, descending=True)[:top_k]
+        masks = t[3][idx].cpu().numpy()
+        classes, scores, boxes = [x[idx].cpu().numpy() for x in t[:3]]
+
+        return masks, boxes
+
+    def get_points(self, mask: np.ndarray, bb_center: tuple[int, int]) -> tuple[tuple[int, int],
+                                                                                tuple[int, int],
+                                                                                tuple[int, int]]:
+        """
+        From a mask and the center of its bounding box, computes three points of interest:
+            - The closest point from the center of the bounding box that is on the mask
+            - The point that is the furthest from the bb center while being on the mask and on the line going through
+              the bounding box center and the first point
+            - The middle point between the two previous points
+        Args:
+            mask (np.ndarray): The mask to process.
+            bb_center (tuple): The center of the mask's bounding box
+
+        Returns:
+            tuple: The three points of interest
+        """
+        center_x, center_y = bb_center
+        # Get the point on the mask that is the closest to the bounding box's center
+        # Super inefficient but should work so....
+        dists = np.empty_like(mask)
+        for i in range(dists.shape[0]):
+            for j in range(dists.shape[1]):
+                dists[i][j] = (center_y - i)**2 + (center_x - j)**2
+        dists = np.where(mask, dists, np.inf)
+        closest_point = np.unravel_index(dists.argmin(), dists.shape)
+        if self.verbose:
+            print(f"{closest_point=}")
+
+        # If the bounding box's center is already on the mask, then just return that
+        if closest_point == (center_y, center_x):
+            if self.verbose:
+                print("Bounding box's center was already on the mask")
+            center = end_point = closest_point
+        # Otherwise, "trace" a line going through the bounding box's center and the closest point on the
+        # mask. We start inspecting that line from the closest point and in the direction going away from
+        # the bounding box's center. Stops when the line goes out of the mask.
+        else:
+            # Get unit vector
+            line_dir = np.asarray([closest_point[0]-center_y, closest_point[1]-center_x])
+            if self.verbose:
+                print(f"line_dir before normalization: {line_dir}")
+            line_dir = line_dir / np.amax(np.abs(line_dir))
+            if self.verbose:
+                print(f"Normalized line_dir: {line_dir}")
+            mult_factor = 1
+            current_x = closest_point[0] + int(line_dir[0] * mult_factor)
+            current_y = closest_point[1] + int(line_dir[1] * mult_factor)
+            while (0 <= current_x < mask.shape[0]
+                    and 0 <= current_y < mask.shape[1]
+                    and mask[current_x, current_y]):
+                current_x = closest_point[0] + int(line_dir[0] * mult_factor)
+                current_y = closest_point[1] + int(line_dir[1] * mult_factor)
+                mult_factor += 1
+
+            center_point = closest_point + (line_dir * mult_factor/2).astype(int)
+            end_point = closest_point + (line_dir * mult_factor).astype(int)
+
+        if self.verbose:
+            print(f"{center=}")
+
+        return closest_point, end_point, center_point
 
 
 def main():
